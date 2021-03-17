f053ab814ec6c48793aa9c266814f98f984de19f
chendongpu <2323178881@qq.com>
2021-03-17 12:00:40 +0800

    实现按需打包antd的样式

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React,{Component} from 'react';
+import {Button,message} from 'antd';
 
 export default class App  extends Component{
+    handleClick=()=>{
+        message.success("asa");
+    }
   render(){
-    return <div>
-          App
-      </div>
+    return <Button type="primary" onClick={this.handleClick} >
+        primary
+    </Button>
   }
 }
 
