13b019c1501412ab468d000ccfa34e7410a2fa9d
chendongpu <2323178881@qq.com>
2021-03-19 09:43:41 +0800

    添加布局

--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -1,6 +1,9 @@
 import React,{Component} from 'react';
 import storeUtils from "../../utils/storeUtils";
 import {Redirect} from 'react-router-dom';
+import { Layout} from 'antd';
+
+const {Header,Footer,Sider,Content} =Layout;
 
 export default class Admin  extends Component{
 
@@ -14,8 +17,15 @@ export default class Admin  extends Component{
         }
 
 
-        return <div >
-            Admin {token}
-        </div>
+        return <Layout style={{height:'100%'}}>
+            <Sider>
+                Sider
+            </Sider>
+            <Layout>
+                <Header>Header</Header>
+                <Content>Content</Content>
+                <Footer>Footer</Footer>
+            </Layout>
+        </Layout>
     }
 }
