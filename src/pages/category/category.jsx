03fc04f0648a1f50dc6930b00ada7044fdde3393
chendongpu <2323178881@qq.com>
2021-03-24 11:29:20 +0800

    完成分类数据的展示

--- src/pages/category/category.jsx
+++ src/pages/category/category.jsx
@@ -11,37 +11,30 @@ import storeUtils from "../../utils/storeUtils";
 
 class Category extends Component {
 
-    async componentWillMount(){
+    state={
+        loading:false,
+        categorys:[]
+    };
 
-
+    getCategory=async ()=>{
+        this.setState({
+            loading:true
+        })
         const response = await reqcategorys(0);
         console.log("请求成功",response);
         if(response.code===0){
-           console.log(response);
+            console.log(response);
+            this.setState({
+                loading:false,
+                categorys:response.result.list
+            })
         }else{
             message.error(response.msg);
         }
-    }
+    };
 
-    componentDidMount() {
-        const { dispatch } = this.props;
-        console.log("dispatch",dispatch);
-        dispatch(getGoodsCategoryList())
-    }
-
-
-    render() {
-
-
-        let title="一级分类";
-
-        const dataSource=[
-            {id:1,name:'美妆'},
-            {id:2,name:'母婴'},
-            {id:3,name:'洗护'}
-        ];
-
-        const columns=[{
+    initColumns=()=>{
+        this.columns=[{
             title:'序号',
             key:'id',
             width:80,
@@ -56,6 +49,7 @@ class Category extends Component {
                 return (
                     <div>
                         <Button type="primary" size="small">修改</Button>
+                        <Button type="primary" size="small" style={{margin:"0 1rem"}}>查看子分类</Button>
                         <Popconfirm title="确定要删除此项么?" onCancel={()=>{console.log("用户取消删除")}} onConfirm={()=>{console.log("用户确认删除")}}>
                             <Button type="danger" style={{margin:"0 1rem"}} size="small">删除</Button>
                         </Popconfirm>
@@ -63,6 +57,27 @@ class Category extends Component {
                 )
             }
         }];
+        this.getCategory();
+    };
+
+     componentWillMount(){
+        this.initColumns()
+    }
+
+    componentDidMount() {
+        const { dispatch } = this.props;
+        console.log("dispatch",dispatch);
+        dispatch(getGoodsCategoryList())
+    }
+
+
+    render() {
+        const {loading,categorys}=this.state;
+
+        let title="一级分类";
+
+        console.log("categorys",categorys);
+
 
         return (
 
@@ -72,7 +87,7 @@ class Category extends Component {
             }
             >
 
-                <Table columns={columns} bordered dataSource={dataSource}/>
+                <Table loading={loading}  pagination={{defaultPageSize:2}}  rowKey="id" columns={this.columns} bordered dataSource={categorys} />
             </Card>
         )
     }
