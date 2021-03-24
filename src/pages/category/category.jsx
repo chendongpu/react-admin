4ed587bc3db3dea17a071e23d3caf369cb9e9653
chendongpu <2323178881@qq.com>
2021-03-24 10:31:55 +0800

    完成分类页面布局

--- src/pages/category/category.jsx
+++ src/pages/category/category.jsx
@@ -4,6 +4,8 @@ import {
     getGoodsCategoryList,
 } from "../../actions/goods/category";
 
+import {Card, Table, Button, Popconfirm, Upload} from 'antd'
+
 class Category extends Component {
 
 
@@ -15,9 +17,47 @@ class Category extends Component {
 
 
     render() {
+        let title="一级分类";
+
+        const dataSource=[
+            {id:1,name:'美妆'},
+            {id:2,name:'母婴'},
+            {id:3,name:'洗护'}
+        ];
+
+        const columns=[{
+            title:'序号',
+            key:'id',
+            width:80,
+            align:'center',
+            render:(txt,record,index)=>index+1
+        },{
+            title:'分类名字',
+            dataIndex:'name'
+        },{
+            title:'操作',
+            render:(txt,record,index)=>{
+                return (
+                    <div>
+                        <Button type="primary" size="small">修改</Button>
+                        <Popconfirm title="确定要删除此项么?" onCancel={()=>{console.log("用户取消删除")}} onConfirm={()=>{console.log("用户确认删除")}}>
+                            <Button type="danger" style={{margin:"0 1rem"}} size="small">删除</Button>
+                        </Popconfirm>
+                    </div>
+                )
+            }
+        }];
+
         return (
 
-                <div>category</div>
+            <Card
+                title={title} extra={
+                <Button type="primary" size="small" >添加分类</Button>
+            }
+            >
+
+                <Table columns={columns} bordered dataSource={dataSource}/>
+            </Card>
         )
     }
 }
