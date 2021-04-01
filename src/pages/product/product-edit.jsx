1f6dbbd1d059a248f71e534eb4220f4222a47cbc
chendongpu <2323178881@qq.com>
2021-04-01 11:04:00 +0800

    校验商品图，添加商品名称表单

--- src/pages/product/product-edit.jsx
+++ src/pages/product/product-edit.jsx
@@ -1,4 +1,5 @@
 import  React,{Component} from 'react';
+import { Form, Input, Button } from 'antd';
 import ImagePicker from "../../components/goods/image-picker";
 import PicturesWall from "../../components/goods/pictures-wall";
 
@@ -31,25 +32,97 @@ export default class ProductEdit extends Component {
     render() {
         const {modalVisible,images,fl}=this.state;
         console.log("images",images);
+
+        const layout = {
+            labelCol: {
+                span: 4,
+            },
+            wrapperCol: {
+                span: 20,
+            },
+        };
+        const tailLayout = {
+            wrapperCol: {
+                offset: 4,
+                span: 20,
+            },
+        };
+
+
+        const onFinish = (values) => {
+            console.log('Success:', values);
+        };
+
+        const onFinishFailed = (errorInfo) => {
+            console.log('Failed:', errorInfo);
+        };
+
+        const checkPictures = (_, value) => {
+            if (images.length > 0) {
+                return Promise.resolve();
+            }
+
+            return Promise.reject(new Error('请选择商品图!'));
+        };
+
         return (
-            <div>
 
 
-                <PicturesWall  fl={images} selectImage={()=>{this.showImage()}} setImages={(tmp)=>{
-                    this.setState({images:tmp});
-                }}/>
 
-                <ImagePicker visible={modalVisible} max={this.maxUid(images)} closeHandler={()=>{this.setState({modalVisible:false});}}   setImages={(imgs)=>{
-                    let tmp =[];
-                    images.map((a)=>{
-                        tmp.push(a);
-                    });
-                    imgs.map((a)=>{
-                        tmp.push(a);
-                    });
-                    this.setState({images:tmp});
-                }}></ImagePicker>
-            </div>
+                <Form
+                    {...layout}
+                    name="basic"
+                    initialValues={{}}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                >
+                    <Form.Item
+                        label="商品图"
+                        name="pictures"
+                        rules={[
+                            {
+                                validator: checkPictures,
+                            },
+                        ]}
+                    >
+                        <PicturesWall  fl={images} selectImage={()=>{this.showImage()}} setImages={(tmp)=>{
+                            this.setState({images:tmp});
+                        }}/>
+                    </Form.Item>
+
+                    <Form.Item
+                        label="商品名称"
+                        name="name"
+                        rules={[
+                            {
+                                required: true,
+                                message: '请输入商品名称!',
+                            },
+                        ]}
+                    >
+                        <Input style={{width: 500}} />
+                    </Form.Item>
+
+
+
+                    <Form.Item {...tailLayout}>
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                    </Form.Item>
+
+                    <ImagePicker visible={modalVisible} max={this.maxUid(images)} closeHandler={()=>{this.setState({modalVisible:false});}}   setImages={(imgs)=>{
+                        let tmp =[];
+                        images.map((a)=>{
+                            tmp.push(a);
+                        });
+                        imgs.map((a)=>{
+                            tmp.push(a);
+                        });
+                        this.setState({images:tmp});
+                    }}></ImagePicker>
+                </Form>
+
         )
     }
 }
